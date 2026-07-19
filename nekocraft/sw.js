82767e8382ac41d0b34df08c0ba2d2f2309f0bee
ms-mcpe <294576626+ms-mcpe@users.noreply.github.com>
2026-07-18 20:41:23 -0400

    Add files via upload

---
+++ nekocraft/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'nekocraft-v1';
+const ASSETS = [
+  './',
+  './instalador-nekocraft.html',
+  './index.html',
+  './home.html',
+  './populars.html',
+  './favorite.html',
+  './downloads.html',
+  './adsterra.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
